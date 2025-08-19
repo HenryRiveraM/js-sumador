b31bb7565c16e0186f0e0a2b1ac4337d0c2098b3
Henry Alejandro Rivera Mendez <henryriveramendez@gmail.com>
2025-08-19 09:29:20 -0400

    Agregando el multiplicador

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,26 @@
 import sumar from "./sumador";
+import multiplicar from "./multiplicador";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
+const multiplicarBtn = document.querySelector("#multiplicar-button");
 
+// Evento para sumar
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  div.innerHTML = "<p>Resultado de la suma: " + sumar(firstNumber, secondNumber) + "</p>";
+});
+
+// Evento para multiplicar
+multiplicarBtn.addEventListener("click", () => {
+  const firstNumber = Number.parseInt(first.value);
+  const secondNumber = Number.parseInt(second.value);
+
+  div.innerHTML = "<p>Resultado de la multiplicación: " + multiplicar(firstNumber, secondNumber) + "</p>";
 });
